f3910dc203f4a3954a15a0de02646e0c2151767c
bastien-winant <armand.winant@gmail.com>
2025-08-04 23:46:06 +0200

    country update function

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -22,14 +22,31 @@ def apply_map(s: pd.Series, map: dict) -> pd.Series:
 	return s.apply(lambda x: map.get(x))
 
 def update_country_list(df, country_col):
-	temp_df = df.loc[:, country_col].drop_duplicates()
-	temp_df['name'] = temp_df[country_col].str.lower()
+	df = df.copy()
+	df[country_col] = df[country_col].str.lower()
+
+	temp_df = df[[country_col]].drop_duplicates()
+	temp_df['country_list_name'] = temp_df[country_col]
 	temp_df.drop(country_col, axis=1, inplace=True)
 
-	country_df = pd.DataFrame(columns=['name'])
+	country_df = pd.DataFrame(columns=['country_list_name'])
+	try:
+		country_df = pd.read_csv('./data/processed/countries.csv')
+	except FileNotFoundError:
+		pass
 
+	country_df = pd.concat([country_df, temp_df])\
+		.drop_duplicates(ignore_index=True)\
+		.reset_index(names='country_list_id')
 
+	df = df\
+		.merge(country_df, how="left", left_on=country_col, right_on='country_list_name')\
+		.rename({'country_list_id': f"{country_col}_id"})\
+		.drop([country_col, 'country_list_name'], axis=1)
 
+	country_df.drop('country_list_id', axis=1).to_csv('./data/processed/countries.csv', index=False)
+
+	return df
 
 def write_csv(df: pd.DataFrame, csv_file_path: str) -> None:
 	df.to_csv(csv_file_path, index=False)
