9e62c897a3f74f33f0e0b7855017aadd090c3ad7
bastien-winant <armand.winant@gmail.com>
2025-08-08 00:02:36 +0200

    add no-ascent-attempt rows

--- src/members.py
+++ src/members.py
@@ -25,25 +25,33 @@ df = df.merge(dim_climber_df, how='left').drop(climber_cols, axis=1)
 
 df.drop([ 'peakid', 'myear', 'mseason', 'age', 'birthdate', 'calcage'], axis=1, inplace=True)
 
-dim_ascent_1_df = df.loc[df.mroute1 > 0, :] \
+df_ascent_0 = df.loc[df.mroute1 == 0, :] \
+	.drop(['mroute1', 'msmtdate1', 'msmttime1', 'mascent1', 'msmtnote1',
+				 'mroute2', 'msmtdate2', 'msmttime2', 'mascent2', 'msmtnote2',
+				 'mroute3', 'msmtdate3', 'msmttime3', 'mascent3', 'msmtnote3'], axis=1)
+df_ascent_0['ascent_number'] = 0
+
+df_ascent_1 = df.loc[df.mroute1 > 0, :] \
 	.drop(['mroute2', 'msmtdate2', 'msmttime2', 'mascent2', 'msmtnote2',
 				 'mroute3', 'msmtdate3', 'msmttime3', 'mascent3', 'msmtnote3'], axis=1) \
 	.rename({'mroute1': 'route', 'msmtdate1': 'summit_date', 'msmttime1': 'summit_time', 'mascent1': 'ascent',
 					 'msmtnote1': 'summit_note'}, axis=1)
-dim_ascent_1_df['ascent_number'] = 1
+df_ascent_1['ascent_number'] = 1
 
-dim_ascent_2_df = df.loc[df.mroute2 > 0, :] \
+df_ascent_2 = df.loc[df.mroute2 > 0, :] \
 	.drop(['mroute1', 'msmtdate1', 'msmttime1', 'mascent1', 'msmtnote1',
 				 'mroute3', 'msmtdate3', 'msmttime3', 'mascent3', 'msmtnote3'], axis=1) \
 	.rename({'mroute2': 'route', 'msmtdate2': 'summit_date', 'msmttime2': 'summit_time', 'mascent2': 'ascent',
 					 'msmtnote2': 'summit_note'}, axis=1)
-dim_ascent_2_df['ascent_number'] = 2
+df_ascent_2['ascent_number'] = 2
 
-dim_ascent_3_df = df.loc[df.mroute3 > 0, :] \
+df_ascent_3 = df.loc[df.mroute3 > 0, :] \
 	.drop(['mroute1', 'msmtdate1', 'msmttime1', 'mascent1', 'msmtnote1',
 				 'mroute2', 'msmtdate2', 'msmttime2', 'mascent2', 'msmtnote2'], axis=1) \
 	.rename({'mroute3': 'route', 'msmtdate3': 'summit_date', 'msmttime3': 'summit_time', 'mascent3': 'ascent',
 					 'msmtnote3': 'summit_note'}, axis=1)
-dim_ascent_3_df['ascent_number'] = 3
+df_ascent_3['ascent_number'] = 3
 
-dim_ascent_df = pd.concat([dim_ascent_1_df, dim_ascent_2_df, dim_ascent_3_df], ignore_index=True)
+print(df.shape)
+df = pd.concat([df_ascent_0, df_ascent_1, df_ascent_2, df_ascent_3], ignore_index=True)
+print(df.shape)
